9c5344912de9ad0acd6fed973ab248e2f538e633
Sam Lu <549429518@qq.com>
2022-03-06 21:55:42 +0800

    Create Luna Projects.

---
+++ src/Compilers/Shared/Luac.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis.CommandLine;
+using Microsoft.CodeAnalysis.ErrorReporting;
+
+namespace SamLu.CodeAnalysis.Lua.CommandLine
+{
+    interface sealed class Luac : LuaCompiler
+    {
+
+    }
+}
