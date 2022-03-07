68b3cbeda0c4d66b3d10ea28882a77464bab4d53
Sam Lu <549429518@qq.com>
2022-03-08 03:11:16 +0800

    temp20220308a
    Starts to build.

---
+++ src/Compilers/Lua/Portable/Syntax/LuaSyntaxNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Syntax;
+
+namespace SamLu.CodeAnalysis.Lua.Syntax
+{
+    public abstract class LuaSyntaxNode : SyntaxNode, IFormattable
+    {
+        internal LuaSyntaxNode(GreenNode green, SyntaxNode? parent, int position) : base(green, parent, position)
+        {
+        }
+
+        internal LuaSyntaxNode(GreenNode green, int position, SyntaxTree syntaxTree) : base(green, position, syntaxTree)
+        {
+        }
+
+        public abstract string ToString(string? format, IFormatProvider? formatProvider);
+    }
+}
