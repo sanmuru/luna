c5c4ace0747aee7ec2c02a6d49a89e60f5acf5e3
Sam Lu <549429518@qq.com>
2022-03-09 00:17:13 +0800

    temp20220309a
    添加LanguageVersion和LuaParseOptions

---
+++ src/Compilers/Lua/Portable/LanguageVersion.cs
@@ -0,0 +1,144 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.CodeAnalysis;
+using Roslyn.Utilities;
+
+namespace SamLu.CodeAnalysis.Lua
+{
+    public enum LanguageVersion
+    {
+        /// <summary>
+        /// Lua语言版本1.0。
+        /// </summary>
+        Lua1 = 1,
+        /// <summary>
+        /// Lua语言版本1.1。
+        /// </summary>
+        Lua1_1,
+        /// <summary>
+        /// Lua语言版本2.1。
+        /// </summary>
+        Lua2_1,
+        /// <summary>
+        /// Lua语言版本2.2。
+        /// </summary>
+        Lua2_2,
+        /// <summary>
+        /// Lua语言版本2.3。
+        /// </summary>
+        Lua2_3,
+        /// <summary>
+        /// Lua语言版本2.4。
+        /// </summary>
+        Lua2_4,
+        /// <summary>
+        /// Lua语言版本2.5。
+        /// </summary>
+        Lua2_5,
+        /// <summary>
+        /// Lua语言版本3.0。
+        /// </summary>
+        Lua3,
+        /// <summary>
+        /// Lua语言版本3.1。
+        /// </summary>
+        Lua3_1,
+        /// <summary>
+        /// Lua语言版本3.2。
+        /// </summary>
+        Lua3_2,
+        /// <summary>
+        /// Lua语言版本4.0。
+        /// </summary>
+        Lua4,
+        /// <summary>
+        /// Lua语言版本5.0。
+        /// </summary>
+        Lua5,
+        /// <summary>
+        /// Lua语言版本5.1。
+        /// </summary>
+        Lua5_1,
+        /// <summary>
+        /// Lua语言版本5.2。
+        /// </summary>
+        Lua5_2,
+        /// <summary>
+        /// Lua语言版本5.3。
+        /// </summary>
+        Lua5_3,
+        /// <summary>
+        /// Lua语言版本5.4。
+        /// </summary>
+        Lua5_4,
+
+        /// <summary>
+        /// 支持的最新的主要版本。
+        /// </summary>
+        LatestMajor = int.MaxValue - 2,
+        /// <summary>
+        /// 下一个预览版本。
+        /// </summary>
+        Preview = int.MaxValue - 1,
+        /// <summary>
+        /// 支持的最新的版本。
+        /// </summary>
+        Latest = int.MaxValue,
+        /// <summary>
+        /// 默认的语言版本，也就是支持的最新的版本。
+        /// </summary>
+        Default = 0
+    }
+
+    internal static class LanguageVersionExtensionsInternal
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static bool IsValid(this LanguageVersion value) => Enum.IsDefined(typeof(LanguageVersion), value);
+
+        internal static ErrorCode GetErrorCode(this LanguageVersion version)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    internal class LuaRequiredLanguageVersion : RequiredLanguageVersion
+    {
+        internal LanguageVersion Version { get; init; }
+
+        internal LuaRequiredLanguageVersion(LanguageVersion version) => this.Version = version;
+
+        public override string ToString() => this.Version.ToDisplayString();
+    }
+
+    public static class LanguageVersionFacts
+    {
+        internal const LanguageVersion LuaNext = LanguageVersion.Preview;
+
+        public static string ToDisplayString(this LanguageVersion version) =>
+            version switch
+            {
+                LanguageVersion.Lua1 => "1.0",
+                LanguageVersion.Lua1_1 => "1.1",
+                LanguageVersion.Lua2_1 => "2.1",
+                LanguageVersion.Lua2_2 => "2.2",
+                LanguageVersion.Lua2_3 => "2.3",
+                LanguageVersion.Lua2_4 => "2.4",
+                LanguageVersion.Lua2_5 => "2.5",
+                LanguageVersion.Lua3 => "3.0",
+                LanguageVersion.Lua3_1 => "3.1",
+                LanguageVersion.Lua3_2 => "3.2",
+                LanguageVersion.Lua4 => "4.0",
+                LanguageVersion.Lua5 => "5.0",
+                LanguageVersion.Lua5_1 => "5.1",
+                LanguageVersion.Lua5_2 => "5.2",
+                LanguageVersion.Lua5_3 => "5.3",
+                LanguageVersion.Lua5_4 => "5.4",
+                LanguageVersion.Default => "default",
+                LanguageVersion.Latest => "latest",
+                LanguageVersion.LatestMajor => "latestmajor",
+                LanguageVersion.Preview => "preview",
+                _ => throw ExceptionUtilities.UnexpectedValue(version)
+            };
+
+#warning 未完成
+    }
+}
--- src/Compilers/Lua/Portable/LuaParseOptions.cs
+++ src/Compilers/Lua/Portable/LuaParseOptions.cs
@@ -1,15 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Text;
 using Microsoft.CodeAnalysis;
+using Roslyn.Utilities;
 
 namespace SamLu.CodeAnalysis.Lua
 {
+    /// <summary>
+    /// 此类型储存数个与解析有关的选项，并且提供修改这些选项的值的方法。
+    /// </summary>
     public sealed class LuaParseOptions : ParseOptions, IEquatable<LuaParseOptions>
     {
         /// <summary>
         /// 默认解析选项。
         /// </summary>
         public static LuaParseOptions Default { get; } = new();
+
+        private ImmutableDictionary<string, string> _features;
+
+        public override IReadOnlyDictionary<string, string> Features => this._features;
+
+        public override string Language => LanguageNames.Lua;
+
+        /// <summary>
+        /// 获取有效的语言版本，编译器将依据版本选择应用程序的语言规范。
+        /// </summary>
+        public LanguageVersion LanguageVersion { get; init; }
+
+        public LanguageVersion SpecifiedLanguageVersion { get; init; }
+
+        internal ImmutableArray<string> PreprocessorSymbols { get; init; }
+
+        public override IEnumerable<string> PreprocessorSymbolNames => this.PreprocessorSymbols;
+
+        public LuaParseOptions(
+            LanguageVersion languageVersion = LanguageVersion.Default,
+            DocumentationMode documentationMode = DocumentationMode.Parse,
+            SourceCodeKind kind = SourceCodeKind.Regular,
+            IEnumerable<string>? preprocessorSymbols = null) :
+                this(languageVersion, documentationMode, kind, preprocessorSymbols.ToImmutableArrayOrEmpty(), ImmutableDictionary<string, string>.Empty)
+        { }
+
+        internal LuaParseOptions(
+            LanguageVersion languageVersion,
+            DocumentationMode documentationMode,
+            SourceCodeKind kind,
+            ImmutableArray<string> preprocessorSymbols,
+            IReadOnlyDictionary<string, string>? features) : base(kind, documentationMode)
+        {
+            this.SpecifiedLanguageVersion = languageVersion;
+            this.LanguageVersion = languageVersion.MapSpecifiedToEffectiveVersion();
+            this.PreprocessorSymbols = preprocessorSymbols.ToImmutableArrayOrEmpty();
+            this._features = features?.ToImmutableDictionary() ?? ImmutableDictionary<string, string>.Empty;
+        }
+
+        private LuaParseOptions(LuaParseOptions other) : this(
+            languageVersion: other.SpecifiedLanguageVersion,
+            documentationMode: other.DocumentationMode,
+            kind: other.Kind,
+            preprocessorSymbols: other.PreprocessorSymbols,
+            features: other.Features)
+        { }
+
+        public sealed override bool Equals(object? obj) => this.Equals(obj as LuaParseOptions);
+        public bool Equals(LuaParseOptions? other)
+        {
+            if (object.ReferenceEquals(this, other)) return true;
+            else if (!base.EqualsHelper(other)) return false;
+            else return this.SpecifiedLanguageVersion == other.SpecifiedLanguageVersion;
+        }
+
+        public override int GetHashCode() =>
+            Hash.Combine(base.GetHashCodeHelper(),
+                Hash.Combine((int)this.SpecifiedLanguageVersion, 0));
     }
 }
