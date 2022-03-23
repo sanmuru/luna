439b6c71340afe3450fe7ba30518c83843f62ec9
Sam Lu <549429518@qq.com>
2022-03-23 17:49:58 +0800

    temp20220323a

--- src/Compilers/Core/Shared/SamLu.CodeAnalysis.Shared/Syntax/InternalSyntax/SyntaxNode.cs
+++ src/Compilers/Core/Shared/SamLu.CodeAnalysis.Shared/Syntax/InternalSyntax/SyntaxNode.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Syntax.InternalSyntax;
@@ -95,6 +96,97 @@ internal abstract class
     public override bool IsStructuredTrivia => this is StructuredTriviaSyntax;
 
     public override bool IsDirective => this is DirectiveTriviaSyntax;
+
+    public override bool IsDocumentationCommentTrivia => SyntaxFacts.IsDocumentationCommentTrivia(this.Kind);
+
+    public SyntaxToken? GetFirstToken() => (SyntaxToken?)this.GetFirstTerminal();
+
+    public SyntaxToken? GetLastToken() => (SyntaxToken?)this.GetLastTerminal();
+
+    public SyntaxToken? GetLastNonmissingToken() => (SyntaxToken?)this.GetLastNonmissingTerminal();
+
+    public virtual GreenNode? GetLeadingTrivia() => null;
+    public sealed override GreenNode? GetLeadingTriviaCore() => this.GetLeadingTrivia();
+
+    public virtual GreenNode? GetTrailingTrivia() => null;
+    public sealed override GreenNode? GetTrailingTriviaCore() => this.GetTrailingTrivia();
+
+    public abstract TResult Accept<TResult>(LuaSyntaxVisitor<TResult> visitor);
+
+    public abstract void Accept(LuaSyntaxVisitor visitor);
+
+#warning SetFactoryContext
+
+    public override Microsoft.CodeAnalysis.SyntaxToken CreateSeparator<TNode>(SyntaxNode element) =>
+#if LANG_LUA
+        Lua.SyntaxFactory.Token(SyntaxKind.CommanToken);
+#elif LANG_MOONSCRIPT
+#error 未实现
+#endif
+
+    public override bool IsTriviaWithEndOfLine() =>
+        this.Kind switch
+        {
+            SyntaxKind.EndOfLineTrivia or
+            SyntaxKind.SingleLineCommentTrivia => true,
+            _ => false
+        };
+
+    /// <summary>
+    /// 使用条件弱表来保存与语法节点相对应的结构语法琐碎内容的唯一实例。
+    /// </summary>
+    private static readonly ConditionalWeakTable<SyntaxNode, Dictionary<Microsoft.CodeAnalysis.SyntaxTrivia, WeakReference<SyntaxNode>>> s_structuresTable = new();
+
+    public override SyntaxNode? GetStructure(Microsoft.CodeAnalysis.SyntaxTrivia trivia)
+    {
+        if (trivia.HasStructure)
+        {
+            var parent = trivia.Token.Parent;
+            if (parent is null)
+                return this.GetNewStructure(trivia);
+            else
+            {
+                SyntaxNode? structure;
+                var structsInParent = s_structuresTable.GetOrCreateValue(parent);
+                lock (structsInParent)
+                {
+                    if (!structsInParent.TryGetValue(trivia, out var weekStructure))
+                    {
+                        structure = this.GetNewStructure(trivia);
+                        structsInParent.Add(trivia, new(structure));
+                    }
+                    else if (!weekStructure.TryGetTarget(out structure))
+                    {
+                        structure = this.GetNewStructure(trivia);
+                        structsInParent.Add(trivia, new(structure));
+                    }
+                }
+
+                return structure;
+            }
+        }
+        else return null;
+    }
+
+#if LANG_LUA
+    /// <summary>
+    /// 使用指定语法琐碎内容创建新的<see cref="Lua.Syntax.StructuredTriviaSyntax"/>实例。
+    /// </summary>
+#elif LANG_MOONSCRIPT
+    /// <summary>
+    /// 使用指定语法琐碎内容创建新的<see cref="MoonScript.Syntax.StructuredTriviaSyntax"/>实例。
+    /// </summary>
+#endif
+    /// <param name="trivia">现有的语法琐碎内容。</param>
+    /// <returns>根据现有的语法琐碎内容创建的新的表示结构语法琐碎内容的语法节点。</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    protected SyntaxNode GetNewStructure(Microsoft.CodeAnalysis.SyntaxTrivia trivia) =>
+#if LANG_LUA
+        Lua
+#elif LANG_MOONSCRIPT
+        MoonScript
+#endif
+        .Syntax.StructuredTriviaSyntax.Create(trivia);
 
 #warning 未完成
 }
--- src/Compilers/Core/Shared/SamLu.CodeAnalysis.Shared/_PREPROCESS_.cs
+++ src/Compilers/Core/Shared/SamLu.CodeAnalysis.Shared/_PREPROCESS_.cs
@@ -2,6 +2,9 @@
 // Lua
 #elif LANG_MOONSCRIPT
 // MoonScript
+#elif LANG_TERRA
+// Terra
+#error 未支持Terra语言
 #else
 #error 不支持的语言
 #endif
