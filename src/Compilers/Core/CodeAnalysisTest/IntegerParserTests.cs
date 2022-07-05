3b375580cd421db418ad83ee778d038c6aa83114
Sam Lu <549429518@qq.com>
2022-07-06 00:07:07 +0800

    Add unit test for SamLu.CodeAnalysis
    1. Test IntegerParser.TryParseDecimalInt64.

---
+++ src/Compilers/Core/CodeAnalysisTest/IntegerParserTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Numerics;
+
+namespace SamLu.CodeAnalysis.UnitTests;
+
+[TestClass]
+public sealed class IntegerParserTests
+{
+    private static readonly Random s_random = new();
+
+    internal static BigInteger RandomBigInteger()
+    {
+        int sign = s_random.Next(2) * 2 - 1;
+        int bits = s_random.Next(2, 310); // 稍微比double.MaxValue大一些。
+        char[] chars = new char[bits];
+        for (int i = 0; i < chars.Length; i++)
+            chars[i] = (char)(s_random.Next(10) + '0');
+        var result = BigInteger.Parse(new string(chars));
+
+        if (sign == -1)
+            return -result;
+        else
+            return result;
+    }
+
+    [TestMethod]
+    public void TryParseDecimalInt64Tests()
+    {
+        Parallel.For(0, 310, body =>
+        {
+            var source = IntegerParserTests.RandomBigInteger();
+            string decimalStr = source.ToString();
+
+            bool succeed = IntegerParser.TryParseDecimalInt64(decimalStr, out var result);
+
+            Assert.IsTrue(source == result);
+
+            if (source > long.MaxValue || source < long.MinValue)
+                Assert.IsFalse(succeed, "大数字超出Int64范围，应当数字溢出导致失败！");
+        });
+    }
+}
