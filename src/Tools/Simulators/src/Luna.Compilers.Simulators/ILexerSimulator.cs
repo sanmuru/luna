020cd7068fa8ae3337bf82c786a208ff13430441
Sam Lu <549429518@qq.com>
2022-07-28 18:54:52 +0800

    Add simulator tools

---
+++ src/Tools/Simulators/src/Luna.Compilers.Simulators/ILexerSimulator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Luna.Compilers.Simulators;
+
+public interface ILexerSimulator
+{
+    IEnumerable<SyntaxToken> LexToEnd(SourceText text);
+
+    IAsyncEnumerable<SyntaxToken> LexToEndAsync(SourceText text, CancellationToken cancellationToken);
+
+    TokenKind GetTokenKind(int rawKind);
+}
---
+++ src/Tools/Simulators/src/Luna.Compilers.Simulators/TokenKind.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luna.Compilers.Simulators;
+
+public enum TokenKind
+{
+    None = 0,
+    Keyword,
+    Operator,
+    Punctuation,
+    NumericLiteral,
+    StringLiteral,
+    WhiteSpace,
+    Comment,
+    Documentation
+}
